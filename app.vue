6b8e86ff446b8f4881540c1dc7b8806c5b07b548
Ricardo Sanginés <rickrebel@gmail.com>
2025-06-20 15:30:14 -0600

    Paso de ts a js

--- app.vue
+++ app.vue
@@ -1,10 +1,17 @@
-<!--<template>-->
-<!--  <div>-->
-<!--    <NuxtRouteAnnouncer />-->
-<!--    <NuxtWelcome />-->
-<!--  </div>-->
-<!--</template>-->
+<script setup>
 
+useSeoMeta({
+  title: 'Observatorio Electoral Judicial',
+  ogTitle: 'Observatorio Electoral Judicial',
+  description: 'Documentamos y analizamos las decisiones, ' +
+          'los problemas y toda la información relevante del proceso electoral ' +
+          'del Poder Judicial 2024-2025',
+  ogDescription: 'Documentamos y analizamos todas las decisiones, ' +
+          'los problemas y toda la información relevante del proceso electoral ' +
+          'del Poder Judicial 2024-2025',
+  ogImage: 'https://oej.yeeko.org/_nuxt/nuevo_logo.ChaL5KSF.png',
+})
+</script>
 <template>
   <NuxtLayout>
     <v-app>
@@ -12,5 +19,3 @@
     </v-app>
   </NuxtLayout>
 </template>
-<script setup lang="ts">
-</script>
